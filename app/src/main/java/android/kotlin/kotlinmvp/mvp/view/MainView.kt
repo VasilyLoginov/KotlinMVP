feab1139a7d84565192a6be9c60f72afd23ce1d1
VasilyLoginov <vloginov83@gmail.com>
2021-03-10 23:56:48 +0300

    Lesson_2_Moxy

--- app/src/main/java/android/kotlin/kotlinmvp/mvp/view/MainView.kt
+++ app/src/main/java/android/kotlin/kotlinmvp/mvp/view/MainView.kt
@@ -1,6 +1,11 @@
 package android.kotlin.kotlinmvp.mvp.view
 
-interface MainView {
+import moxy.MvpView
+import moxy.viewstate.strategy.AddToEndSingleTagStrategy
+import moxy.viewstate.strategy.StateStrategyType
+
+@StateStrategyType(AddToEndSingleTagStrategy::class)
+interface MainView: MvpView {
     fun setButtonOneText(text: String)
     fun setButtonTwoText(text: String)
     fun setButtonThreeText(text: String)
